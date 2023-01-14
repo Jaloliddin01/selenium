cceb480c6603ab492888f0ab186ba804b566c014
Jaloliddin <jaloliddinmurodullayev337@gmail.com>
2023-01-14 15:19:20 +0500

    Page navigating and Clicking

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 
@@ -9,10 +12,27 @@ driver.get('https://www.techwithtim.net/')
 
 print(driver.title)
 
-sr = driver.find_element(By.NAME, 's')
-sr.send_keys('test')
-sr.send_keys(Keys.RETURN)
+link = driver.find_element(By.LINK_TEXT, 'Python Programming')
+link.click()
 
-time.sleep(5)
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.LINK_TEXT, "Beginner Python Tutorials"))
+    )
+    element.click()
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'sow-button-19310003'))
+    )
+    element.click()
 
-driver.quit()
+    driver.back()
+    driver.back()
+    driver.back()
+
+    driver.forward()
+    driver.forward()
+
+    time.sleep(10)
+
+finally:
+    driver.quit()
